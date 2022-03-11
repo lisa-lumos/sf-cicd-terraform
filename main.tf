3b0fab284801356daf2f3e8765c545f8cc5f312e
lisa-lumos <zjcty307@gmail.com>
2022-03-10 22:00:54 -0700

    Update

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "example-org-57dd1a"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
